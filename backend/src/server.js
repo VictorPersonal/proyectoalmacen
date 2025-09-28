abad773b460a4782fc43b57acced3b3c54cc04e6
Victor Personal <tabareztor2005@gmail.com>
2025-09-27 19:58:08 -0500

    Aqui modifique la estructura del back, donde lo configure para la base de datos creando 2 carpetas scr/config y alli coloque .env y db.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+
 app.get('/', (req, res) => {
-  res.send('Backend funcionando 🚀');
+  res.send('Servidor funcionando con PostgreSQL 🚀');
 });
 
+
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
