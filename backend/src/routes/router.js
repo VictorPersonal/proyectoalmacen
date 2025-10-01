34bafc959772a650fb8d4fda33a070dc46b9d6d3
Victor Personal <tabareztor2005@gmail.com>
2025-10-01 17:46:06 -0500

    Problemas para backend

---
+++ backend/src/routes/router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../config/db');
+
+router.get('/', (req, res) => {
+    pool.query('SELECT * FROM clienteusuario',(error,result)=>{
+        if(error){
+            throw error;
+        }
+        else{
+            res.send(result);
+        }
+    })
+})
+
+module.exports = router;
