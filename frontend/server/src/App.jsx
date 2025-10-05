abff52e457b4b4d8e52f415fcdd3b22fcc1618bf
MaicolZzz <buitragomaicol23@gmail.com>
2025-10-05 16:55:22 -0500

    Agregue login y styles, rutas de login

--- frontend/server/src/App.jsx
+++ frontend/server/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Registro from "./pages/Registro";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/registro" element={<Registro />} />
+        <Route path="/login" element={<Login />} />
+
       </Routes>
     </Router>
   );
