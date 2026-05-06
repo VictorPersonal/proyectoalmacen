0e2a70e2c47fbf0287067bd583b46ba843a3419a
Victor Personal <tabareztor2005@gmail.com>
2026-05-05 22:54:33 -0500

    El contenido de la clase auth-links esta bien posicionado

--- frontend/server/src/pages/Home.jsx
+++ frontend/server/src/pages/Home.jsx
@@ -404,75 +404,6 @@ const Home = () => {
             </button>
           </div>
 
-          <div className="nav-links" id="nav-links"> {/*Se deve mover el contenido de este div*/ }
-            <div className="categorias-menu">
-              <button className="nav-link categorias-btn" onClick={toggleMenu}>
-                Categorías ∨
-              </button>
-
-              {menuAbierto && (
-                <div className="menu-desplegable">
-                  <div className="categorias-lista">
-                    {categorias.map((categoria, index) => (
-                      <div key={index} className="categoria-item-container">
-                        {categoria.subcategorias.length > 1 ? (
-                          <div
-                            className="categoria-item con-submenu"
-                            onMouseEnter={() => toggleSubmenu(index)}
-                            onMouseLeave={() => setSubmenuAbierto(null)}
-                          >
-                            <span>{categoria.nombre}</span>
-                            <FaChevronRight className="submenu-icon" />
-
-                            {submenuAbierto === index && (
-                              <div className="submenu">
-                                {categoria.subcategorias.map((sub, i) => (
-                                  <button
-                                    key={i}
-                                    className="submenu-item"
-                                    onClick={() => {
-                                      cargarProductosPorCategoria(sub.id);
-                                      setMenuAbierto(false);
-                                    }}
-                                  >
-                                    {sub.nombre}
-                                  </button>
-                                ))}
-                              </div>
-                            )}
-                          </div>
-                        ) : (
-                          <button
-                            className="categoria-item"
-                            onClick={() => {
-                              cargarProductosPorCategoria(
-                                categoria.subcategorias[0].id
-                              );
-                              setMenuAbierto(false);
-                            }}
-                          >
-                            {categoria.nombre}
-                          </button>
-                        )}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              )}
-            </div>
-
-            <Link to="/promociones" className="nav-link">
-              Promociones
-            </Link>
-            <Link to="/contacto" className="nav-link">
-              Contacto
-            </Link>
-            <a href="#" className="nav-link">
-              Ayuda
-            </a>
-          </div>
-        </nav>
-
         <div className="auth-links" id="auth-links"> {/*Se deve mover el contenido de este div*/ }
           {usuarioLogueado ? (
             <div className="perfil-menu">
@@ -531,6 +462,10 @@ const Home = () => {
             <FaShoppingCart />
           </div>
         </div>
+
+
+        </nav>
+
       </header>
 
       {/* CARRITO */}
